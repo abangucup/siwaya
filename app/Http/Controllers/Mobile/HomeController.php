e8f269dd76a46404d2d1034167cd285707372050
salmanmustapa <id.salmanmustapa@gmail.com>
2024-11-15 05:48:11 +0800

    Update Tampilan Home

--- app/Http/Controllers/Mobile/HomeController.php
+++ app/Http/Controllers/Mobile/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Mobile;
 
 use App\Http\Controllers\Controller;
+use App\Models\Wbtb;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -15,6 +16,21 @@ class HomeController extends Controller
 
     public function home()
     {
-        return view('mobile.home');
+        $totalDiverifikasi = Wbtb::where('status', 'diverifikasi')->count();
+        $totalDitetapkan = Wbtb::where('status', 'ditetapkan')->count();
+        $totalDitolak = Wbtb::where('status', 'ditolak')->count();
+        $totalDiajukan = Wbtb::where('status', 'diajukan')->count();
+
+        $wbtbDitetapkan = Wbtb::where('status', 'ditetapkan')->latest()->take(3)->get();
+        $wbtbDiverifikasi = Wbtb::where('status', 'diverifikasi')->latest()->take(3)->get();
+
+        $data = [
+            'WBTB Diverifikasi' => $totalDiverifikasi,
+            'WBTB Ditetapkan' => $totalDitetapkan,
+            'WBTB Ditolak' => $totalDitolak,
+            'WBTB Diajukan' => $totalDiajukan
+        ];
+
+        return view('mobile.home', compact('data', 'wbtbDiverifikasi', 'wbtbDitetapkan'));
     }
 }
